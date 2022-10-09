7841056a389ee2374d060b2e434a7a1749831fa2
euotten <49993923+euotten@users.noreply.github.com>
2022-10-09 16:26:29 +0200

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Ernst-Ulrich Otten on 09.10.22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
